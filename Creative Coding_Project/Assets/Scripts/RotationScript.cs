847dd54e90263fbbb818130ca4d9635109ff1b13
avegai <50087438+avegai@users.noreply.github.com>
2021-02-24 14:15:19 +0100

    Added project to github

---
+++ Creative Coding_Project/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+   public float speed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+             
+    }
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+
+        transform.Rotate(Vector3.down * speed * 5 * Time.deltaTime);
+
+        //transform.localScale = new Vector3(Random.RandomRange(-1f, 1f), 0.5f, 0.5f);
+    }
+}
+ 
---
+++ Creative Coding_Project/Assets/Scripts/SphereColor.cs
@@ -0,0 +1,41 @@
+﻿//Attach this script to any GameObject in your scene to spawn a cube and change the material color
+using UnityEngine;
+
+public class SphereColor : MonoBehaviour
+{
+    private float _color;
+    public Material dissolveMat;
+    //public float Opacity;
+    //public float maxOpacity;
+
+    public float c_Color
+
+    {
+        get
+        {
+            return _color;
+        }
+
+        private set
+        {
+            _color = value;
+        }
+    }
+
+    void Start()
+    {
+        c_Color = 10f;
+        Debug.Log(c_Color);
+
+        dissolveMat.SetFloat("_DitherSize", c_Color);
+         
+    }
+
+    void Update()
+
+    {
+        dissolveMat.SetFloat("_DitherSize", c_Color);
+       //transform.localScale = new Vector3(c_Color, c_Color, c_Color);
+
+    }
+}
