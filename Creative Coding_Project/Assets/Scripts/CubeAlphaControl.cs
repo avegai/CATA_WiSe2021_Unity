379fb9e8745821e270dc699cb2cfcc7482ebbaf4
avegai <50087438+avegai@users.noreply.github.com>
2021-02-28 22:12:42 +0100

    Added an Input script to the Cube to mess with the see Thru Factor

---
+++ Creative Coding_Project/Assets/Scripts/CubeAlphaControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAlphaControl : MonoBehaviour
+{
+    private float alphaCube;
+    public Material seeThruFactor;
+    
+    void Start()
+    {
+        // seeThruFactor.SetFloat("_AlphaFactor", a_Alpha);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            seeThruFactor.SetFloat("_AlphaFactor", Random.Range(0.1f, 0.9f));
+        }
+            
+    }
+}
